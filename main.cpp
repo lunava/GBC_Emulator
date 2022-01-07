7f1a0a11c8695d5ecb4aa6ffaffbdc42a077292f
= <=>
2022-01-06 18:16:48 -0800

    added project directory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "include/gbc/cpu/CPU.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    CPU test(10);
+
     return 0;
 }
---
+++ src/cpu/CPU.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Luis Navarrete Rios on 1/6/22.
+//
+
+#include "gbc/cpu/CPU.h"
+#include "iostream"
+
+ CPU::CPU(int x) {
+    std::cout << x << std::endl;
+}
