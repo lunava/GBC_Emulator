973d9da455094161a22269872c1d6b1048c198bd
eirik64 <70987628+eirik64@users.noreply.github.com>
2022-01-08 14:14:05 -0800

    MMU header complete

---
+++ include/memory/Memory.h
@@ -0,0 +1,28 @@
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "common.h"
+
+class Memory {
+  const Byte io_reset[0x100];
+
+  Byte cartridge[0x8000];
+  Byte sram[0x2000];
+  Byte io[0x100];
+  Byte vram[0x2000];
+  Byte oam[0x100];
+  Byte wram[0x2000];
+  Byte hram[0x80];
+
+  void copy(Word destination, Word source, size_t length);
+
+  Byte readByte(Word address);
+  Word readShort(Word address);
+  Word readShortFromStack(void);
+
+  void writeByte(Word address, Byte value);
+  void writeShort(Word address, Word value);
+  void writeShortToStack(Word value);
+}
+#endif //MEMORY_H
